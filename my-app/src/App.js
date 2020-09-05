8b5847fd991c711753c3ff3c17880c0d6a24bc75
jazz_iron <dlwocjf90@gmail.com>
2020-09-05 22:27:22 +0900

    리액트 part1 - part 11
    리액트 part1 - part 11

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component, useState } from 'react';
+//import MyComponent from './MyComponent';
+//import Counter from './Counter';
+//import Say from './Say';
+//import ValidationSample from './ValidationSample';
+//import ScrollBox from './ScrollBox';
+//import IterationSample from './IterationSample';
+//import LifeCycleSample from './LifeCycleSample';
+
+import Info from './Info';
+//import Average from './Average';
+
+
+
+
+
+const App = () => {
+  
+  return < Info/>
+}
+
+
+// function getRandomColor() {
+//   return '#' + Math.floor(Math.random() * 16777215).toString(16);
+// }
+
+// class App extends Component {
+//   state = {
+//     color : '#000000'
+//   }
+
+//   handleClick = () => {
+//     this.setState({
+//       color: getRandomColor()
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>랜덤 색상</button>
+//         <LifeCycleSample color={this.state.color} />
+//       </div>
+//     )
+//   }
+// }
+
+export default App;
