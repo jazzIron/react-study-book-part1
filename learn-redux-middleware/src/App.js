3d7aec2feee1ca50a77564ea2a63d4b9ae411eea
jazz_iron <dlwocjf90@gmail.com>
2020-10-01 23:10:18 +0900

    part18
    part18

---
+++ learn-redux-middleware/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SampleContainer from './containers/SampleContainer';
+//import CounterContainer from './containers/CounterContainer';
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  )
+}
+
+export default App;
